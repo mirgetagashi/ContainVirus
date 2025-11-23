ca3769b7ec4e331b291efe1c1888775a2940edb6
Mirgeta Gashi <mirgeta.gashi@tretek.io>
2025-11-23 23:17:28 +0100

    changes in solution and logs

--- ContainVirus/Solution/Solution.cs
+++ ContainVirus/Solution/Solution.cs
@@ -5,131 +5,135 @@ namespace ContainVirus.Solution
 {
     public class Solution
     {
-        static readonly int[] DR = { 1, -1, 0, 0 };
-        static readonly int[] DC = { 0, 0, 1, -1 };
+        private static readonly int[] DR = { 1, -1, 0, 0 };
+        private static readonly int[] DC = { 0, 0, 1, -1 };
+
+        private class RegionInfo
+        {
+            public int Id;
+            public int FrontierCount;
+            public int WallCount;
+        }
 
         public int ContainVirus(int[][] grid)
         {
-            int m = grid.Length, n = grid[0].Length, N = m * n;
-            int answer = 0;
-            var q = new int[N];          
-            int[] seen1 = new int[N];      
-            int[] seen0 = new int[N];      
-            int seen1Tok = 1;            
-            int regionTokStart = 10;      
+            int m = grid.Length;
+            int n = grid[0].Length;
 
-            var regionCells = new List<int>();         
-            var regionCellsIndex = new List<int>();    
-            var cells = new List<int>(N);             
-            var frontiers = new List<List<int>>();    
-            var walls = new List<int>();               
+            int[,] regionId = new int[m, n];
+            bool[] contained = new bool[m * n + 5];
+
+            var frontierQueue = new Queue<(int r, int c, int region)>();
+
+            int totalWalls = 0;
+            int nextRegionId = 1;
 
             while (true)
             {
-                seen1Tok++;
-                regionCells.Clear();
-                regionCellsIndex.Clear();
-                cells.Clear();
-                frontiers.Clear();
-                walls.Clear();
+                int roundStartRegionId = nextRegionId;
+                int bestRegionId = 0;
+                int bestFrontierSize = 0;
+                int wallsForBestRegion = 0;
+
+                var infoPerRegion = new Dictionary<int, RegionInfo>();
 
                 for (int r = 0; r < m; r++)
                 {
                     for (int c = 0; c < n; c++)
                     {
-                        if (grid[r][c] != 1) continue;
-                        int idx = r * n + c;
-                        if (seen1[idx] == seen1Tok) continue;
-                        int qh = 0, qt = 0;
-                        q[qt++] = idx;
-                        seen1[idx] = seen1Tok;
-                        int start = cells.Count;
-                        int wallCnt = 0;
-                        var frontier = new List<int>();
+                        if (grid[r][c] == 0) continue;
 
-                        int regionStamp = regionTokStart + frontiers.Count + 1;
+                        if (contained[regionId[r, c]]) continue;
 
-                        while (qh < qt)
+                        if (regionId[r, c] >= roundStartRegionId) continue;
+
+                        int currentRegionId = nextRegionId++;
+
+                        if (contained.Length <= currentRegionId)
                         {
-                            int cur = q[qh++];
-                            cells.Add(cur);
-                            int cr = cur / n, cc = cur % n;
-                            for (int d = 0; d < 4; d++)
-                            {
-                                int nr = cr + DR[d], nc = cc + DC[d];
-                                if ((uint)nr >= (uint)m || (uint)nc >= (uint)n) continue;
-                                int nid = nr * n + nc;
-                                int val = grid[nr][nc];
-
-                                if (val == 1)
-                                {
-                                    if (seen1[nid] != seen1Tok)
-                                    {
-                                        seen1[nid] = seen1Tok;
-                                        q[qt++] = nid;
-                                    }
-                                }
-                                else if (val == 0)
-                                {
-                                    wallCnt++;
-                                    if (seen0[nid] != regionStamp)
-                                    {
-                                        seen0[nid] = regionStamp;
-                                        frontier.Add(nid);
-                                    }
-                                }
-
-                            }
+                            Array.Resize(ref contained, contained.Length * 2);
                         }
-                        regionCellsIndex.Add(start);
-                        frontiers.Add(frontier);
-                        walls.Add(wallCnt);
+
+                        var info = new RegionInfo { Id = currentRegionId };
+
+                        DFS(r, c, grid, m, n,
+                            regionId, contained,
+                            roundStartRegionId, info,
+                            frontierQueue);
+
+                        infoPerRegion[currentRegionId] = info;
+
+                        if (info.FrontierCount > bestFrontierSize)
+                        {
+                            bestFrontierSize = info.FrontierCount;
+                            bestRegionId = currentRegionId;
+                            wallsForBestRegion = info.WallCount;
+                        }
                     }
                 }
 
-                if (frontiers.Count == 0) break;
+                if (bestFrontierSize == 0) break;
 
-                int best = -1, bestFront = 0;
-                for (int i = 0; i < frontiers.Count; i++)
+                totalWalls += wallsForBestRegion;
+                contained[bestRegionId] = true;
+
+                while (frontierQueue.Count > 0)
                 {
-                    int f = frontiers[i].Count;
-                    if (f > bestFront)
+                    var (r, c, region) = frontierQueue.Dequeue();
+
+                    if (region == bestRegionId) continue;
+
+                    if (grid[r][c] == 0)
                     {
-                        bestFront = f;
-                        best = i;
+                        grid[r][c] = 1;
+                        regionId[r, c] = region;
                     }
                 }
-
-                if (bestFront == 0) break; 
-
-
-                answer += walls[best];
-                {
-                    int start = regionCellsIndex[best];
-                    int end = best + 1 < regionCellsIndex.Count ? regionCellsIndex[best + 1] : cells.Count;
-                    for (int k = start; k < end; k++)
-                    {
-                        int id = cells[k];
-                        int rr = id / n, cc = id % n;
-                        grid[rr][cc] = -1;
-                    }
-                }
-
-
-                for (int i = 0; i < frontiers.Count; i++)
-                {
-                    if (i == best) continue;
-                    foreach (var id in frontiers[i])
-                    {
-                        int rr = id / n, cc = id % n;
-                        if (grid[rr][cc] == 0) grid[rr][cc] = 1;
-                    }
-                }
-
-                regionTokStart += frontiers.Count + 5;
             }
 
-            return answer;
+            return totalWalls;
+        }
+
+        private void DFS(
+            int r,
+            int c,
+            int[][] grid,
+            int m,
+            int n,
+            int[,] regionId,
+            bool[] contained,
+            int roundStartRegionId,
+            RegionInfo info,
+            Queue<(int r, int c, int region)> frontierQueue)
+        {
+            if (r < 0 || c < 0 || r >= m || c >= n) return;
+
+            if (grid[r][c] == 1)
+            {
+                if (regionId[r, c] >= roundStartRegionId || contained[regionId[r, c]]) return;
+
+                regionId[r, c] = info.Id;
+
+                for (int d = 0; d < 4; d++)
+                {
+                    DFS(r + DR[d], c + DC[d],
+                        grid, m, n,
+                        regionId, contained,
+                        roundStartRegionId, info,
+                        frontierQueue);
+                }
+            }
+            else if (grid[r][c] == 0)
+            {
+                info.WallCount++;
+
+                if (regionId[r, c] != info.Id)
+                {
+                    info.FrontierCount++;
+                    regionId[r, c] = info.Id;
+                    frontierQueue.Enqueue((r, c, info.Id));
+                }
+            }
         }
     }
 }
--- ContainVirus/Solution/SolutionWithLogs.cs
+++ ContainVirus/Solution/SolutionWithLogs.cs
@@ -6,134 +6,108 @@ namespace ContainVirus.Solution
 {
     public class SolutionWithLogs
     {
-        static readonly int[] DR = { 1, -1, 0, 0 };
-        static readonly int[] DC = { 0, 0, 1, -1 };
+        private static readonly int[] DR = { 1, -1, 0, 0 };
+        private static readonly int[] DC = { 0, 0, 1, -1 };
+
+        private class RegionInfo
+        {
+            public int Id;
+            public int FrontierCount;
+            public int WallCount;
+            public List<(int r, int c)> Cells = new List<(int r, int c)>();
+        }
 
         public (int totalWalls, int totalDays, List<VirusStepLog> logs) ContainVirusWithLogs(int[][] grid)
         {
-            int m = grid.Length, n = grid[0].Length, N = m * n;
-            int answer = 0;
-            var logs = new List<VirusStepLog>();
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            int[,] regionId = new int[m, n];
+            bool[] contained = new bool[m * n + 5];
+
+            var frontierQueue = new Queue<(int r, int c, int region)>();
+
+            int totalWalls = 0;
+            int nextRegionId = 1;
             int day = 0;
-
-            var q = new int[N];
-            int[] seen1 = new int[N];
-            int[] seen0 = new int[N];
-            int seen1Tok = 1;
-            int regionTokStart = 10;
-
-            var regionCellsIndex = new List<int>();
-            var cells = new List<int>(N);
-            var frontiers = new List<List<int>>();
-            var walls = new List<int>();
+            var logs = new List<VirusStepLog>();
 
             while (true)
             {
                 day++;
-                seen1Tok++;
-                regionCellsIndex.Clear();
-                cells.Clear();
-                frontiers.Clear();
-                walls.Clear();
+                int roundStartRegionId = nextRegionId;
+                int bestRegionId = 0;
+                int bestFrontierSize = 0;
+                int wallsForBestRegion = 0;
+
+                var infoPerRegion = new Dictionary<int, RegionInfo>();
+
+                frontierQueue.Clear();
 
                 for (int r = 0; r < m; r++)
                 {
                     for (int c = 0; c < n; c++)
                     {
-                        if (grid[r][c] != 1) continue;
-                        int idx = r * n + c;
-                        if (seen1[idx] == seen1Tok) continue;
+                        if (grid[r][c] == 0) continue;
 
-                        int qh = 0, qt = 0;
-                        q[qt++] = idx;
-                        seen1[idx] = seen1Tok;
+                        if (contained[regionId[r, c]]) continue;
 
-                        int start = cells.Count;
-                        int wallCnt = 0;
-                        var frontier = new List<int>();
-                        int regionStamp = regionTokStart + frontiers.Count + 1;
+                        if (regionId[r, c] >= roundStartRegionId) continue;
 
-                        while (qh < qt)
+                        int currentRegionId = nextRegionId++;
+
+                        if (contained.Length <= currentRegionId)
                         {
-                            int cur = q[qh++];
-                            cells.Add(cur);
-                            int cr = cur / n, cc = cur % n;
-
-                            for (int d = 0; d < 4; d++)
-                            {
-                                int nr = cr + DR[d], nc = cc + DC[d];
-                                if ((uint)nr >= (uint)m || (uint)nc >= (uint)n) continue;
-                                int nid = nr * n + nc;
-                                int val = grid[nr][nc];
-
-                                if (val == 1)
-                                {
-                                    if (seen1[nid] != seen1Tok)
-                                    {
-                                        seen1[nid] = seen1Tok;
-                                        q[qt++] = nid;
-                                    }
-                                }
-                                else if (val == 0)
-                                {
-                                    wallCnt++;
-                                    if (seen0[nid] != regionStamp)
-                                    {
-                                        seen0[nid] = regionStamp;
-                                        frontier.Add(nid);
-                                    }
-                                }
-                            }
+                            Array.Resize(ref contained, contained.Length * 2);
                         }
 
-                        regionCellsIndex.Add(start);
-                        frontiers.Add(frontier);
-                        
-                        walls.Add(wallCnt);
+                        var info = new RegionInfo { Id = currentRegionId };
+
+                        DFS(r, c, grid, m, n,
+                            regionId, contained,
+                            roundStartRegionId, info,
+                            frontierQueue);
+
+                        infoPerRegion[currentRegionId] = info;
+
+                        if (info.FrontierCount > bestFrontierSize)
+                        {
+                            bestFrontierSize = info.FrontierCount;
+                            bestRegionId = currentRegionId;
+                            wallsForBestRegion = info.WallCount;
+                        }
                     }
                 }
 
-                if (frontiers.Count == 0) break;
+                if (bestFrontierSize == 0) break;
 
-                int best = -1, bestFront = 0;
-                for (int i = 0; i < frontiers.Count; i++)
-                {
-                    if (frontiers[i].Count > bestFront)
-                    {
-                        bestFront = frontiers[i].Count;
-                        best = i;
-                    }
-                }
-                if (bestFront == 0) break;
+                totalWalls += wallsForBestRegion;
+                contained[bestRegionId] = true;
 
-                answer += walls[best];
                 var log = new VirusStepLog
                 {
                     DayNumber = day,
-                    ChosenRegionWalls = walls[best]
+                    ChosenRegionWalls = wallsForBestRegion
                 };
 
-                int startIdx = regionCellsIndex[best];
-                int endIdx = best + 1 < regionCellsIndex.Count ? regionCellsIndex[best + 1] : cells.Count;
-                for (int k = startIdx; k < endIdx; k++)
+                var bestRegion = infoPerRegion[bestRegionId];
+                foreach (var (r, c) in bestRegion.Cells)
                 {
-                    int id = cells[k];
-                    int rr = id / n, cc = id % n;
-                    grid[rr][cc] = -1;
-                    log.ChosenRegionCells.Add((rr, cc));
+                    grid[r][c] = -1;
+                    log.ChosenRegionCells.Add((r, c));
                 }
 
-                for (int i = 0; i < frontiers.Count; i++)
+                while (frontierQueue.Count > 0)
                 {
-                    if (i == best) continue;
-                    foreach (var id in frontiers[i])
+                    var (r, c, region) = frontierQueue.Dequeue();
+
+                    if (region == bestRegionId) continue;
+
+                    if (grid[r][c] == 0)
                     {
-                        int rr = id / n, cc = id % n;
-                        if (grid[rr][cc] == 0)
-                        {
-                            grid[rr][cc] = 1;
-                            log.SpreadCells.Add((rr, cc));
-                        }
+                        grid[r][c] = 1;
+                        regionId[r, c] = region;
+                        log.SpreadCells.Add((r, c));
                     }
                 }
 
@@ -145,11 +119,52 @@ namespace ContainVirus.Solution
                 }
                 log.GridSnapshot = snapshot;
                 logs.Add(log);
-
-                regionTokStart += frontiers.Count + 5;
             }
 
-            return (answer, day, logs);
+            return (totalWalls, day, logs);
+        }
+
+        private void DFS(
+            int r,
+            int c,
+            int[][] grid,
+            int m,
+            int n,
+            int[,] regionId,
+            bool[] contained,
+            int roundStartRegionId,
+            RegionInfo info,
+            Queue<(int r, int c, int region)> frontierQueue)
+        {
+            if (r < 0 || c < 0 || r >= m || c >= n) return;
+
+            if (grid[r][c] == 1)
+            {
+                if (regionId[r, c] >= roundStartRegionId || contained[regionId[r, c]]) return;
+
+                regionId[r, c] = info.Id;
+                info.Cells.Add((r, c));
+
+                for (int d = 0; d < 4; d++)
+                {
+                    DFS(r + DR[d], c + DC[d],
+                        grid, m, n,
+                        regionId, contained,
+                        roundStartRegionId, info,
+                        frontierQueue);
+                }
+            }
+            else if (grid[r][c] == 0)
+            {
+                info.WallCount++;
+
+                if (regionId[r, c] != info.Id)
+                {
+                    info.FrontierCount++;
+                    regionId[r, c] = info.Id;
+                    frontierQueue.Enqueue((r, c, info.Id));
+                }
+            }
         }
     }
 }
